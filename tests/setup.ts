c8301f6d5e5e8362854b38cf1681928524dd9f0f
asgarovf <fasgarov2002@gmail.com>
2025-04-30 21:44:54 +0300

    fix: remove solana from test env

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,9 @@
 import { createConfig } from '../src/createConfig'
-import { EVM, Solana } from '../src/index'
+import { EVM } from '../src/index'
 
 export const setupTestEnvironment = () => {
   createConfig({
     integrator: 'lifi-sdk',
-    providers: [EVM(), Solana()],
+    providers: [EVM()],
   })
 }
